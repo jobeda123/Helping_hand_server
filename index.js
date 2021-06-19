5ffbe1091e4ea4a66fb3c9ad8e2eed4e2d107a3a
Jobeda Nur <jobedanur265@gmail.com>
2021-06-19 14:46:01 +0600

    mongoDB passed Updated

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Yes, I am connecting.....')
 })
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
